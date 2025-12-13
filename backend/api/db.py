acec325392b694e970d625761e859a323defec83
Philip-cam <jp.12221221cambridge@icloud.com>
2025-12-13 17:13:11 +0900

    todoapp仮実装, mysqlとの接続は現時点で不可

---
+++ backend/api/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import getpass
+
+username = input("Enter your username : ")
+password = getpass.getpass("Enter your password : ")
+
+ASYNC_DB_URL = f"mysql+aiomysql://{username}:{password}@db:3306/demo?charset=utf8"
+
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,62 @@
-from fastapi import FastAPI
+# uvicorn backend.api.main:app --reload --reload --workers 1 --host 0.0.0.0 --port 8000
+# http://localhost:8000/docs
+from fastapi import FastAPI, Request, status, Query, Header, Response, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+from .routers import task, done
 
 app = FastAPI()
+app.include_router(task.router)
+app.include_router(done.router)
+
+
+@app.exception_handler(RequestValidationError)
+async def handler(request:Request, exc:RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 @app.get("/hello")
 async def hello():
     return {"message": "hello world!"}
 
-# uvicorn backend.api.main:app --reload --reload --workers 1 --host 0.0.0.0 --port 8000
-# http://localhost:8000/docs
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/greet")
+async def greet_user(name: str ="ゲスト"):
+    return {"message": f"こんにちは、{name}さん！"}
+
+@app.get("/search/")
+async def search_items(keyword: str = Query(..., min_length=2, max_length=20, description="検索ワード")):
+    return {"result": f"'{keyword}' で検索しました"}
+
+@app.get("/users/{user_id}")
+async def get_user_data(user_id: int, detail: bool = False):
+    return {"user_id": user_id, "detail": detail}
+
+class User(BaseModel):
+    username: str
+    email: str
+    age: int
+
+@app.post("/users/")
+async def create_user(user: User):
+    return {"message": f"{user.username}さんを登録しました", "email": user.email}
+
+@app.get("/header-check")
+async def check_header(user_agent: str = Header(default=None)):
+    return {"user_agent": user_agent}
+
+@app.get("/custom-header")
+async def custom_header(response: Response):
+    response.headers["X-Custom-Header"] = "これはカスタムヘッダーです"
+    return {"message": "ヘッダーを設定しました"}
+
+@app.get("/secure-data")
+async def secure_data(token: str = Header(...)):
+    if token != "mysecrettoken":
+        raise HTTPException(status_code=403, detail="認証失敗")
+    return {"data": "これは保護された情報です"}
