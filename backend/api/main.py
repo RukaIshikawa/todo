ec61092cf94dbe1b34eb28636c406480688c1c74
RukaIshikawa <rukaisi13@gmail.com>
2025-12-21 20:42:51 +0900

    first change of backend

--- backend/api/main.py
+++ backend/api/main.py
@@ -12,9 +12,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],  # 開発中だけ
+    allow_origins=["*"],  # 開発中だけ
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
